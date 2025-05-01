842b87110343620664dfeaf78d2d5ec0efd46e62
rohinifegade <rohini.fegade01@gmail.com>
2025-05-01 08:59:31 -0700

    Task 1 commiting

--- Task1.py
+++ Task1.py
@@ -1,1 +1,10 @@
-# Task 1: Read a File and Handle Errors .
+# Task 1: Read a File and handle errors.
+
+try:
+    with open('sample.txt', 'r') as file:
+        print("Reading file content:")
+        for line_number, line in enumerate(file, start=1):
+            print(f"Line {line_number}: {line.strip()}")
+            #print("Line " + str(line_number) + ": " + line.strip())  
+except FileNotFoundError:
+    print("Error: The file sample.txt was not found.")
